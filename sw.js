49ef328caa7d04e7861a03eee9d170724f64daaa
padilla585projects <padilla585.projects@gmail.com>
2026-04-16 06:53:22 +0200

    v4.3: bump cache alejandra-v4.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.2';
+const CACHE = 'alejandra-v4.3';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
