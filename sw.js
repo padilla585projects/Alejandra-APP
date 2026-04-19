6fcd39e84a6abafc50d655a9301f67e5195e56f7
padilla585projects <padilla585.projects@gmail.com>
2026-04-19 22:54:45 +0200

    v4.41: bump versión, actualizar txts

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.40';
+const CACHE = 'alejandra-v4.41';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
