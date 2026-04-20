66608b651f8987e107fd4bf2689d25510bbd6436
padilla585projects <padilla585.projects@gmail.com>
2026-04-20 19:33:17 +0200

    v4.50: fix Google login (version-check race condition + boton incorrecto en callback)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.49';
+const CACHE = 'alejandra-v4.50';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
