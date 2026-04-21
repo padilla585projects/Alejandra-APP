23c04cc613857589301cfde8fefae920583a4fd1
padilla585projects <padilla585.projects@gmail.com>
2026-04-21 14:48:15 +0200

    Fix banner actualizacion: extrae version de SW_ACTUALIZADO; v4.64

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.63';
+const CACHE = 'alejandra-v4.64';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
