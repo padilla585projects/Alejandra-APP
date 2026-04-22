05756752e0602cfa87322bd332bd6bc362508a14
padilla585projects <padilla585.projects@gmail.com>
2026-04-22 20:39:24 +0200

    fix: v4.74 aislamiento dept - sync sesion D1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.73';
+const CACHE = 'alejandra-v4.74';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
