5fc18e1ea2191505c1f9fc59198d67165e6e4c7c
padilla585projects <padilla585.projects@gmail.com>
2026-04-22 20:44:55 +0200

    fix: v4.75 corregir bucle SW_ACTUALIZADO

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.74';
+const CACHE = 'alejandra-v4.75';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
