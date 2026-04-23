eb42bcaaa3ed63e3d1db4be16db275ff0912314b
padilla585projects <padilla585.projects@gmail.com>
2026-04-23 22:10:48 +0200

    v4.89 - BUG-009 reducir consumo bateria: intervalos optimizados + pausa en background

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.88';
+const CACHE = 'alejandra-v4.89';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
