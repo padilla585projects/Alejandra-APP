c4f2d039548bc9f34fb78dd1f4bc685b9c55c7a0
padilla585projects <padilla585.projects@gmail.com>
2026-04-23 22:14:15 +0200

    v4.90 - NEW-02 wizard de bienvenida (onboarding primera vez)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.89';
+const CACHE = 'alejandra-v4.90';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
