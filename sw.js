f0da2694f2f76947474b1d7b8e58fc6900be248c
padilla585projects <padilla585.projects@gmail.com>
2026-04-23 22:07:32 +0200

    v4.88 - MEJ-137 renombrar carpetas completo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v4.87';
+const CACHE = 'alejandra-v4.88';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
