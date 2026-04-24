c65653d902591b284ca2bcc56678424c5d6b5a8a
padilla585projects <padilla585.projects@gmail.com>
2026-04-24 21:56:43 +0200

    v5.09: tarjetas home reordenables (drag-and-drop + touch, localStorage)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v5.08';
+const CACHE = 'alejandra-v5.09';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
