f76a86be67b56803b3a5bdb70ac35c77dfba74c9
APEX Agent <apex@padilla585.dev>
2026-06-25 12:53:30 +0200

    feat: Informe de Estado de Obra + v6.93

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v6.92';
+const CACHE = 'alejandra-v6.93';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
