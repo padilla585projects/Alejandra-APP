8811ed8c86d10e0608c001d956a5c850a812f1f6
APEX Agent <apex@padilla585.dev>
2026-07-18 19:31:06 +0200

    fix: mejorar logica inicializarEstadoSidebar - principal e inventarios siempre abiertos - v7.94

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v7.93';
+const CACHE = 'alejandra-v7.94';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
