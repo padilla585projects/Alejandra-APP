8eb19f862fc7d20b378e9edf07aea9f801d50a0b
APEX Agent <apex@padilla585.dev>
2026-07-18 19:24:49 +0200

    fix: ocultacion sidebar en DOMContentLoaded para garantizar ejecutarse - v7.91

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v7.90';
+const CACHE = 'alejandra-v7.91';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
