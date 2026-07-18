01f65ab0c015220aa0b2fafe6477e24180ec3cd1
APEX Agent <apex@padilla585.dev>
2026-07-18 12:38:26 +0200

    feat: panel office - reorganizar sidebar (Seguridad, Obra) + FAB Alejandra chat - v7.87

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v7.86';
+const CACHE = 'alejandra-v7.87';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
