8ab4d74955c43cb67447ca42a2d010d82a7341d4
APEX Agent <apex@padilla585.dev>
2026-07-18 19:30:02 +0200

    feat: inicializar sidebar con todos cerrados excepto inventarios - v7.93

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v7.92';
+const CACHE = 'alejandra-v7.93';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
