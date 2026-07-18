af1080228a35db88749547de0e1d1c3ab3a8f3a1
APEX Agent <apex@padilla585.dev>
2026-07-18 12:24:40 +0200

    ui: ocultar tambien secciones Obra y Seguimiento del sidebar para no-admin - v7.86

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v7.85';
+const CACHE = 'alejandra-v7.86';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
