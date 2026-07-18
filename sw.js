9bd54500bc821425b887cad98df4c9bfefc3ba85
APEX Agent <apex@padilla585.dev>
2026-07-18 19:23:09 +0200

    fix: ocultacion sidebar por permisos con delay para evitar timing issue - v7.90

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v7.89';
+const CACHE = 'alejandra-v7.90';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
