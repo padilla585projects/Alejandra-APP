86388b59f39c95034d46c596b963bbe2b8e86c76
padilla585projects <padilla585.projects@gmail.com>
2026-07-25 12:09:18 +0200

    feat: boton flotante chat equipo (SCAN-04c) + confirma columna Nº albaran en bobinas - v8.28

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v8.27';
+const CACHE = 'alejandra-v8.28';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
