0d246d0710da47d807dbd59bf55bb3b23e321f1c
padilla585projects <padilla585.projects@gmail.com>
2026-07-25 11:35:20 +0200

    fix: separa el boton flotante de DevTools IA del de Alejandra (SCAN-04) - v8.25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v8.24';
+const CACHE = 'alejandra-v8.25';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
