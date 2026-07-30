a0d833037668a8c931e81026e78a3c55f4252ea8
padilla585projects <padilla585.projects@gmail.com>
2026-07-30 18:59:30 +0200

    fix: improve telecom cable category contrast

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v8.86';
+const CACHE = 'alejandra-v8.87';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
