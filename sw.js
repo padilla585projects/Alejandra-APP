8ad404b586771414db2b32bac8a84d3b15c320d5
padilla585projects <padilla585.projects@gmail.com>
2026-08-12 08:39:12 +0200

    chore: bump version a 9.06 para la entrega de hoy
    Sincronizado en los 4 marcadores (version.json, sw.js, index.html, panel.html)
    antes de publicar en Pages.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia este número cada vez que actualices la app
-const CACHE = 'alejandra-v9.05';
+const CACHE = 'alejandra-v9.06';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
